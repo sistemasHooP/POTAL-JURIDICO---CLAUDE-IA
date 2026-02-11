1f65652f9807568b1260bb33afb446a0b82bc89c
sistemasHooP <thiegosistemas@gmail.com>
2026-02-11 13:18:23 -0300

    Update cliente-config.js

--- cliente/js/cliente-config.js
+++ cliente/js/cliente-config.js
@@ -8,7 +8,7 @@
 const CONFIG_CLIENTE = {
     // URL da API (mesma do sistema principal)
     // IMPORTANTE: Substitua pela URL da sua implantação do Google Apps Script
-    API_URL: "https://script.google.com/macros/s/AKfycbwi-M_wVyhadUQEySSFyQRxD66Xv6pACGQUcArMJLwxGt0aGlAACBGCJDTp4_HX7Lpj/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbysNj8BfIlGqNFkSV8tJ2pPjWD3Xi0UFVBBfJPQnHqd3t4h5RtqzQK3DgxzJXtdPLks/exec",
 
     // Nome do aplicativo
     APP_NAME: "Acompanhamento Processual",
@@ -38,4 +38,5 @@ const CONFIG_CLIENTE = {
 // Congela para evitar modificações acidentais
 Object.freeze(CONFIG_CLIENTE);
 Object.freeze(CONFIG_CLIENTE.STORAGE_KEYS);
+
 Object.freeze(CONFIG_CLIENTE.PAGES);
